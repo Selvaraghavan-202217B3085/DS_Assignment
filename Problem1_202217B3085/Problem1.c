4f1a4a63b1e0a4c74b8f5497fd6f6c603f1e602b
Selvaraghavan-202217B3085 <202217B3085@wilp.bits-pilani.ac.in>
2024-07-21 17:14:43 +0000

    Add files via upload

---
+++ Problem1_202217B3085/Problem1.c
@@ -0,0 +1,133 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 10
+/* Define the required functions to create a list, insert into the list,
+delete an element from the list, search and display the list */
+void create();
+void insert();
+void deletion();
+void search();
+void display();
+int a,b[20], n, p, e, f, i, pos;
+void main()
+{
+//clrscr();
+int ch;
+char g='y';
+do
+{
+printf("\n***main Menu***");
+printf("\n1.Create\n2.Delete\n3.Search\n4.Insert\n5.Display\n6.Exit\n");
+printf("\n Enter your Choice:");
+scanf("%d", &ch);
+/* The following switch will call the appropriate choice provided
+by the user */
+switch(ch)
+{
+case 1:
+create();
+break;
+case 2:
+deletion();
+break;
+case 3:
+search();
+break;
+case 4:
+insert();
+break;
+case 5:
+display();
+break;
+case 6:exit(1);
+break;
+default:
+printf(" \n Enter the correct choice:");
+}
+printf("\n Do u want to continue:::");
+scanf("\n%c", &g);
+}
+/* The program is intended to run till we provide inputs other that ‘y’
+or ‘Y’ */
+while(g=='y'||g=='Y');
+}
+void create()
+{
+printf("\n Enter the number of nodes:");
+scanf("%d", &n);
+/* The loop should run till we get the ‘n’ inputs */
+for(i=0;i<n;i++)
+ {
+{
+printf("\n Enter the Element:",i+1);
+scanf("%d", &b[i]);
+}
+}
+}
+void deletion()
+{
+printf("\n Enter the position u want to delete::");
+scanf("%d", &pos);
+if(pos>=n)
+{
+printf("\n Invalid Location::");
+}
+else
+{
+for(i=pos+1;i<n;i++)
+{
+b[i-1]=b[i];
+}n--;
+}
+printf("\n The Elements after deletion");
+for(i=0;i<n;i++)
+{
+printf("\t%d", b[i]);
+}
+}
+void search()
+{
+printf("\n Enter the Element to be searched:");
+scanf("%d", &e);
+for(i=0;i<n;i++)
+{
+if(b[i]==e)
+{
+printf("Value is in the %d Position", i);
+}
+else
+{
+printf("Value %d is not in the list::", e);
+continue;
+}
+}
+}
+void insert()
+{
+printf("\n Enter the position u need to insert::");
+scanf("%d", &pos);
+if(pos>=n)
+{
+printf("\n invalid Location::");
+}
+else
+{
+for(i=MAX-1;i>=pos-1;i--)
+{
+b[i+1]=b[i];
+}
+printf("\n Enter the element to insert::\n");
+scanf("%d",&p);
+b[pos]=p;
+n++;
+}
+printf("\n The list after insertion::\t");
+display();
+}
+
+void display()
+{
+ printf("\n The Elements of The list ADT are:");
+  for(i=0;i<n;i++)
+ printf("\n%d\t", b[i]);
+}
